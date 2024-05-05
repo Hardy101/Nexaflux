14ee853349587e08094393388ebf051772ebade4
Eghosa Ordia <eghordia130@gmail.com>
2024-05-05 05:25:34 +0100

    apps.py uncomment important variables

--- main/tasks.py
+++ main/tasks.py
@@ -1,38 +1,6 @@
 from django.conf import settings
 import requests
-import json
-import random
 from datetime import datetime
-
-
-postcodes = [
-	"SW1A 1AA",
-	"PE35 6EB", 
-	"CV34 6AH",
-	"EH1 2NG"
-]
-
-def schedule_api():
-
-	postcode = postcodes[random.randint(0, 3)]
-
-	full_url = f"https://api.postcodes.io/postcodes/{postcode}"
-			
-	r = requests.get(full_url)
-	if r.status_code == 200:
-
-		result = r.json()["result"]
-
-		lat = result["latitude"]
-		lng = result["longitude"]
-
-		print(f'Latitude: {lat}, Longitude: {lng}')
-
-		#77779
-
-
-def check_api_endpoint():
-    pass
 
 
 def still_valid():
@@ -43,9 +11,10 @@ def still_valid():
 
     for item in data:
         timestamp_str = item['validuntil']
+        status = item['status']
         timestamp = datetime.fromisoformat(timestamp_str)
 
-        if datetime.now():
+        if datetime.now() and status == 'active':
               due_entries.append(item)
     return due_entries
 
