5af6548fd5e56197a16e05293d1e9f6c5f54c2a3
Eghosa Ordia <eghordia130@gmail.com>
2024-05-05 18:02:40 +0100

    scheduler debug

--- main/tasks.py
+++ main/tasks.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 import requests
 from datetime import datetime
+from django.utils import timezone
 from alerts_and_data.models import Alert
 
 ENDPOINT = 'https://api.npoint.io/fc1045cc362997a2adb3'
@@ -10,6 +11,7 @@ def still_valid():
     response = requests.get(ENDPOINT)
     data = response.json()
 
+    current_time = timezone.now()
     due_entries = []
 
     for item in data:
@@ -17,37 +19,34 @@ def still_valid():
         status = item['status']
         timestamp = datetime.fromisoformat(timestamp_str)
 
-        if datetime.now() and status == 'active':
-              due_entries.append(item)
+        if timestamp > current_time and status == 'active':
+            due_entries.append(item)
              
     return due_entries
 
 def send_sms():
-      for entry in still_valid():
-            print(entry['status'], entry['coin'])
-
+    for entry in still_valid():
+        print(entry['status'], entry['coin'])
 
 def check_price():
-    entries = []
-    if still_valid:
-        for item in still_valid():
-            id = item['id']
-            target = item['target']
-            coin = item['coin']
-            recipient = item['recipient']
-            params = {
-            'fsym': coin,
-            'tsyms': 'USDT'
-            }
-            # data = requests.get(PRICE_ENDPOINT, params=params).json()
-            # price = data['USDT']
-            print(f'congrats🎉 dear {recipient}, Your target of {target}USDT has been met,')
-            alert_instance = Alert.objects.get(pk=id)
-            alert_instance.status = 'fulfilled'
-            alert_instance.save()
-            # if int(float(target)):
-            #     entries.append(item)
-    else:
-        print('Sorry, Nothing here...')
-    # print(entries)
+    for item in still_valid():
+        id = item['id']
+        target = item['target']
+        coin = item['coin']
+        recipient = item['recipient']
+        params = {'fsym': coin, 'tsyms': 'USDT'}
 
+        # Fetching price data
+        # data = requests.get(PRICE_ENDPOINT, params=params).json()
+        # price = data['USDT']
+        
+        # Printing message
+        print(f'congrats🎉 dear {recipient}, Your target of {target}USDT has been met,')
+
+        # Updating status to 'fulfilled'
+        alert_instance = Alert.objects.get(pk=id)
+        alert_instance.status = 'fulfilled'
+        alert_instance.save()
+
+# if __name__ == "__main__":
+#     check_price()
